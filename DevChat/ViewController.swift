107c9aadd227320c303516e2b82aa572079d5f38
Apostolos Chalkias <achalkias@hotmail.com>
2017-08-08 12:08:44 +0300

    Initial Commit

---
+++ DevChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DevChat
+//
+//  Created by Apostolos Chalkias on 08/08/2017.
+//  Copyright © 2017 Apostolos Chalkias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
